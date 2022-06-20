e2d566dc572a68cebe315f3c7fe4d8bd8db5fd0a
Yury Frolov <yura200253@gmail.com>
2022-06-20 20:33:57 +0500

    Added max memory bench

--- src/internal/benchmarks/wal_fetch_max_mem.py
+++ src/internal/benchmarks/wal_fetch_max_mem.py
@@ -1,15 +1,14 @@
 import logging
 import os
-import time
 from typing import List
 
 from internal.base.base_benchmark import BaseBenchmark
 from internal.base.result import Result
 from utils.exceptions.benchmarks import BenchmarkNotRanResults, BenchmarkNotConfigured, BenchmarkError
-from utils.walg.run_walg_command import run_walg_command
+from utils.walg.run_walg_command import time_walg_command
 
 
-class RunWalFetchNTimes(BaseBenchmark):
+class RunWalFetchNTimesMaxMem(BaseBenchmark):
     def __init__(self, times: int):
         self.times = times
         self._results = []
@@ -22,14 +21,15 @@ class RunWalFetchNTimes(BaseBenchmark):
                 ' this info must be provided by one of the wrappers in field pushed_wal_name'
             )
 
-        total_duration = 0
-        times = 0
+        total, times = 0, 0
+
         for i in range(self.times):
             if os.path.isfile('./random.wal'):
                 os.remove('./random.wal')
 
-            ret_code, out, err = run_walg_command(
-                f'/usr/bin/time -f "%M" wal-fetch {self.pushed_wal_name} ./random.wal'
+            ret_code, out, err = time_walg_command(
+                f'wal-fetch {self.pushed_wal_name} ./random.wal',
+                '%M'
             )
 
             if ret_code != 0:
@@ -37,9 +37,11 @@ class RunWalFetchNTimes(BaseBenchmark):
                 logging.error(f'stdout: {out}; stderr: {err}')
                 raise BenchmarkError('nonzero return code in walg run')
 
-            print()
+            mem = int(out.split('\n')[-1])
+            total += mem
+            times += 1
 
-        self._results.append(Result(f'avg_time', 1))
+        self._results.append(Result(f'max_mem_kb', total / times))
 
     def results(self) -> List[Result]:
         if not self._results:
